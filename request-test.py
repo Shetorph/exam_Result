6f5bed6fe852dfb69572e0d15aaf2fcb0de9da2c
Eren Çakır <erencakkr28@gmail.com>
2024-12-09 09:12:14 +0300

    first commit

---
+++ request-test.py
@@ -0,0 +1,67 @@
+import requests
+import time
+from bs4 import BeautifulSoup
+
+#Sınır 3saat. Sonrasında cookie yenilenmesi lazım.
+#1saat 60dakika
+#1gün 1440dakika
+#50dakikada 1tekrar yapılıyor. 1gün çalışması için i<=28.8
+#2gün için i<=28.8*2
+#Bu hesaplamayla kaç gün çalıştıracağınızı belirleyin.
+
+print("Lütfen cookie'yi giriniz.")
+cookieValue = str(input())
+dersler = []
+i=0
+while(i<=28):
+    
+    dersler.clear()
+    headers = {
+        "accept": "*/*",
+        "cookie": cookieValue,
+        "cache-control": "no-cache",
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "origin": "https://obs.dpu.edu.tr",
+        "referer": "https://obs.dpu.edu.tr/oibs/std/index.aspx?curOp=0"
+    }
+
+
+    URl = "https://obs.dpu.edu.tr/oibs/std/not_listesi_op.aspx"
+    response = requests.get(URl, headers=headers)
+    soup = BeautifulSoup(response.content, "html.parser")
+    rows = soup.select("#grd_not_listesi tr")[1:]
+    
+    
+    for row in rows:
+        cells = row.find_all("td")
+        if len(cells) > 7:
+            ders_adi = cells[2].text.strip()
+
+            # Vize notunu al
+            vize_element = cells[4].find("span", string=lambda x: "Vize" in x if x else False)
+            vize = None
+            if vize_element:
+                vize = vize_element.text.split(":")[-1].strip()
+
+            # Ort, Not ve Durum değerlerini al
+            ort = cells[5].text.strip()
+            not_degeri = cells[6].text.strip()
+            durum = cells[7].text.strip()
+
+            dersler.append({
+                "ders": ders_adi,
+                "vize": vize,
+                "ort": ort,
+                "not": not_degeri,
+                "durum": durum
+            })
+
+    # Sonuçları yazdır
+    print("↓" * 75)
+    for ders in dersler:
+        print(f"Ders : {ders['ders']}")
+        print("-" * 25)
+        print(f"Vize: {ders['vize']}, Ort: {ders['ort']}, Not: {ders['not']}, Durum: {ders['durum']}\n")
+    print("↑" * 75)
+    time.sleep(1800)
+    i+=1
